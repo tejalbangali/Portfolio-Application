e5f65095f099837daddb97c6a40fa37ad3640ea1
Tejal Bangali <tejal.bangali@gmail.com>
2023-12-15 06:56:48 +0530

    responsive hamburger nav done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
 import styles from "./Navbar.module.css";
 import NavList from "./NavList";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 
 const Navbar = () => {
+  const [showMediaIcon, setShowMediaIcon] = useState(false);
+
+  const setMediaIcon = () => {
+    setShowMediaIcon(!showMediaIcon);
+  };
+
   return (
     <nav className={styles.nav}>
       <div className={styles.logoName}>
@@ -9,13 +18,22 @@ const Navbar = () => {
           <img src="/public/logo.png" alt="" /> Tejal Bangali
         </a>
       </div>
-      <div className={styles.navList}>
+
+      <div
+        className={showMediaIcon ? styles.viewMobNavList :  styles.mobNavList}
+      >
         <ul>
           <NavList text="HOME" link="#" />
           <NavList text="ABOUT" link="#" />
           <NavList text="PROJECTS" link="#" />
           <NavList text="CONTACT" link="#" />
         </ul>
+      </div>
+
+      <div className={styles.hamburger}>
+        <a href="#" onClick={() => setMediaIcon()}>
+          {showMediaIcon ? <CloseRoundedIcon /> : <MenuRoundedIcon />}
+        </a>
       </div>
     </nav>
   );
